1bb8d0960a72c6a188ca1fac7e99f83a613ab04b
Dinnall <michaeldinnall@gmail.com>
2018-04-06 22:52:09 -0400

    switched static API to productJSON and added sketch images

--- src/api/shop.js
+++ src/api/shop.js
@@ -1,11 +1,31 @@
 /**
  * Mocking client-server processing
  */
-import _products from './products.json'
+// import _products from './products.json'
 
-const TIMEOUT = 100
+
+
+
+// Replace static API for fetched JSON
+
+let inventoryImages = [
+ {inventoryImage: 'chronograph.jpg'},
+ {inventoryImage: 'quartz.jpg'},
+ {inventoryImage: 'weekender.jpg'}
+]
 
 export default {
-  getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
-  buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
+  getProducts: (cb) => {
+    fetch("http://tech.work.co/shopping-cart/products.json")
+    .then((data) => data.json())
+    .then(inventory =>{
+
+      inventory.map((item, index) =>{
+         item.inventoryImages = inventoryImages[index].inventoryImage
+        });
+      console.log("List all inventory w/ image :",inventory)
+      cb(inventory)
+    })
+  },
+  buyProducts: (payload, cb) => cb()
 }
